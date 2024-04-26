ed2c8f57931c4b306250e1bb558e7899b8fe2b2c
jumozaga <jumozaga@yahoo.com>
2024-04-26 02:31:39 -0300

    Add exemplo de react router dom

--- vnw-desafio-final/portifolio_blog/README.md
+++ vnw-desafio-final/portifolio_blog/README.md
@@ -1,8 +1,8 @@
-# React + Vite
+# Desafio Final
+#### Kleber Matos - Vai Na Web
+Seu blog pessoal deve ser simples, mas poderoso, com links para suas redes sociais, projetos incríveis que você já realizou e tecnologias que domina. É momento de compartilhar suas realizações com o mundo! 💪👩‍💻👨‍💻
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Vite + React + Bun
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+##### Exemplo de uso do react-router-dom
+src -> App.js
---
+++ vnw-desafio-final/portifolio_blog/src/App.js
@@ -0,0 +1,36 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">Sobre nós</Link></li>
+            <li><Link to="/contact">Contato</Link></li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
